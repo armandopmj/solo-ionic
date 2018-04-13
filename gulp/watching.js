4a6846c27678d668237636256a79486fa999a80e
Armando Perez <armandopmj@gmail.com>
2018-04-13 13:50:39 -0500

    chore(gulp): add old gulp scripts for referance

---
+++ gulp/watching.js
@@ -0,0 +1,84 @@
+'use strict';
+// gulp
+var gulp = require('gulp');
+
+var paths = gulp.paths;
+var options = gulp.options;
+// modules
+var bs = require('browser-sync').create();
+var proxyMiddleware = require('http-proxy-middleware');
+
+var bsInit = function(paths, openOverride) {
+    var bsOptions = {
+        server: {
+            baseDir: paths
+        }
+    };
+    if (options.env === 'proxy') {
+        bsOptions.server.middleware = proxyMiddleware('/api', {
+            target: 'http://SOME_PROXY_URL.com',
+            changeOrigin: true
+        });
+    }
+    if (options.open === false) {
+        bsOptions.open = false;
+    }
+    if (openOverride !== undefined) {
+        bsOptions.open = openOverride;
+    }
+    bs.init(bsOptions);
+};
+
+// WATCH
+gulp.task('watch', ['inject-all'], function() {
+
+    // browser sync server
+    bsInit(['app', '.tmp']);
+
+    var watchFiles = paths.jsFiles
+        .concat([
+            'app/index.html',
+            'app/*/assets/**/*'
+        ])
+        .concat(paths.templates);
+
+    // start linting and watching
+    gulp.start('linting');
+    gulp.watch(watchFiles, function(event) {
+        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+        if (event.type === 'changed') {
+            bs.reload();
+            gulp.start('linting');
+        } else { // added or deleted
+            // inject in index (implicitly reloads)
+            gulp.start('inject-all');
+        }
+    });
+    // watch for changes in scss
+    // FIXME watching styles is reloading twice
+    gulp.watch('app/**/*/*.scss', ['styles', 'inject-all']);
+    // watch for changes in environment files and new config files
+    gulp.watch([
+        'app/config/*'
+    ], ['environment']);
+});
+
+// WATCH-BUILD
+var watchBuildDeps = [];
+if (options.build !== false) {
+    watchBuildDeps.push('build');
+}
+gulp.task('watch-build', watchBuildDeps, function() {
+    bsInit(paths.dist);
+    gulp.watch(paths.dist + '**/*', function() {
+        bs.reload();
+    });
+});
+
+// SERVE TASKS
+gulp.task('serve', ['inject-all'], function() {
+    bsInit(['app', '.tmp'], false);
+});
+gulp.task('serve-build', ['build'], function() {
+    bsInit(['app', '.tmp'], false);
+});
